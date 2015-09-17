41a1e94e97ef56d93286337c5a9e430c5f9e591b
andru <andrush001@gmail.com>
2015-09-17 22:08:44 +0200

    Jelszó emlékeztető hozzáadva.

---
+++ src/main/java/pl/controllers/NewPasswordController.java
@@ -0,0 +1,25 @@
+package pl.controllers;
+
+/**
+ * Created by andru on 9/17/2015.
+ */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import pl.MessageBox;
+
+
+public class NewPasswordController {
+
+    @FXML
+    private TextField mailField;
+
+    @FXML
+    private void handleSend() {
+        if(mailField.getText() == "") {
+            System.out.println("Empty");
+        }
+    }
+
+
+}
