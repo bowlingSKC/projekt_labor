146ac444c8f30ba85aff3accee711a07d909343e
andru <andrush001@gmail.com>
2015-09-21 20:07:18 +0200

    A véletlen generált jelszó bevitele az adatbázisba.

--- src/main/java/pl/controllers/ForgotPasswordController.java
+++ src/main/java/pl/controllers/ForgotPasswordController.java
@@ -6,7 +6,17 @@ package pl.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import pl.Main;
+import pl.MessageBox;
 import pl.SendMail;
+import pl.jpa.SessionUtil;
+import pl.model.User;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
 
 public class ForgotPasswordController {
@@ -18,18 +28,53 @@ public class ForgotPasswordController {
     String password = "projektlabor";                   //our password
     String recipiantEmail;                              //destination
     String title = "Password remainder";                //subject
-    String message = "message test";                    //message
+    String message;                                     //message
+    private SecureRandom random;
+    public String nextSessionId() {
+        return new BigInteger(130, random).toString(32);
+    }
 
     @FXML
     private void handleSend() {
+        message = "Your new password: ";
         recipiantEmail = mailField.getText();
-        if(mailField.getText() == "") {
-            System.out.println("Empty");
+        if(mailField.getText().length() == 0) {
+            System.out.println("Üres mez?.");
         }
-        SendMail.Send(username, password, recipiantEmail, title, message);
+        random = new SecureRandom();                    //generate random number
+        String newpassword = nextSessionId();           //save password to a String
+        newpassword = newpassword.subSequence(0,8).toString();
+        System.out.println(newpassword);
+        message = message + newpassword;
 
+        try {
+            String newSalt = Main.getSalt();
+            String newPass = Main.getSHA512Hash(newpassword, newSalt);
+            changePassword(newPass, newSalt, recipiantEmail);
+            System.out.println(message);
+            mailField.setText("Please wait!");
+            SendMail.Send(username, password, recipiantEmail, title, message);
+            mailField.setText("E-mail sent!");
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+            MessageBox.showErrorMessage("Hiba", "A jelszóemlékeztet? elküldése sikertelen volt!",
+                    "Próbálja kés?bb.", false);
+        }
 
     }
 
+    private void changePassword(String newPass, String newSalt, String recipiantEmail) {
+        Session session = SessionUtil.getSession();
+        Criteria criteria = session.createCriteria(User.class);
+        User mailUser = (User) criteria.add(Restrictions.eq("email", recipiantEmail)).uniqueResult();
+        System.out.println(mailUser.getLastname() + " " + mailUser.getFirstname());
+        mailUser.setSalt(newSalt);
+        mailUser.setPassword(newPass);
+        org.hibernate.Transaction tx = session.beginTransaction();
+        session.update(mailUser);
+        tx.commit();
+        session.close();
+
+    }
 
 }
